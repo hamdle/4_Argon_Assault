f1c3c95bde1a4981eb02d52b5250babcb79854a7
Eric Marty <contact@ericmarty.co>
2017-11-09 21:53:44 -0800

    Ship movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,6 +6,7 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour {
 
     [Tooltip("in meters per second (ms^-1)")][SerializeField] float xSpeed = 4f;
+    [SerializeField] float xRange = 5f;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +17,8 @@ public class Player : MonoBehaviour {
 	void Update () {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
-        print(xOffset);
+        float xPosRaw = transform.localPosition.x + xOffset;
+        float xPosClamp = Mathf.Clamp(xPosRaw, -xRange, xRange);
+        transform.localPosition = new Vector3(xPosClamp, transform.localPosition.y, transform.localPosition.z);
 	}
 }
